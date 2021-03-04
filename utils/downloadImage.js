79d2f5929b1aca9607f5aef667e36c81bc916121
smithd-roses <smithd.contact@gmail.com>
2021-03-04 23:58:09 +0200

    remove console

--- utils/downloadImage.js
+++ utils/downloadImage.js
@@ -1,7 +1,6 @@
 const fetch = require("node-fetch");
 
 module.exports = async function downloadImage(url) {
-  console.log(url);
   if (!url) throw new Error('Provide url');
   try {
     const options = { headers: { accept: 'image/x-png' } };
